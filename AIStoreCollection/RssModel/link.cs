3e6b6c3eb2196ae48d2b4919d02078b6ecec683d
ThaiTeam <izikl@live.com>
2015-05-29 03:43:57 -0700

    -adding link to RSSModel

---
+++ AIStoreCollection/RssModel/link.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace AIStoreCollection.RssModel
+{
+    public class link
+    {
+        [XmlAttribute]
+        public string href { get; set; }
+    }
+}
